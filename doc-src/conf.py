9da56216007d1ec6c2c26d5883154e69104af695
Trevor Campbell <trevor@kelpie9.shartrec.com>
2023-11-28 11:08:29 +1100

    Update documentation theme

--- doc-src/conf.py
+++ doc-src/conf.py
@@ -6,7 +6,7 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'Kelpie Flight Planner'
+project = 'Kelpie' ## 'Kelpie Flight Planner'
 copyright = '2023, Trevor Campbell'
 author = 'Trevor Campbell'
 release = '0.1'
@@ -18,11 +18,27 @@ extensions = []
 
 templates_path = ['_templates']
 exclude_patterns = []
-
+source_suffix = ".rst"
+master_doc = "index"
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'nature'
+html_theme = 'alabaster'
 html_static_path = ['_static']
+html_sidebars = {
+    '**': [
+        'about.html',
+        'navigation.html',
+        'relations.html',
+        'searchbox.html',
+        'donate.html',
+    ]
+}
+html_theme_options = {
+    "description": "A Flight Planner for Flightgear",
+    "github_user": "shartrec",
+    "github_repo": "kelpie-flight-planner",
+    "fixed_sidebar": True,
+}
