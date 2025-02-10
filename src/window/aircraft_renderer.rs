040c2f8896e8ad7f0d24a2b1a88ac5174967f298
Trevor Campbell <trevor@shartrec.com>
2025-02-10 14:03:24 +1100

    Draw a nice plane on map

--- src/window/aircraft_renderer.rs
+++ src/window/aircraft_renderer.rs
@@ -80,9 +80,8 @@ impl AircraftRenderer {
     }
 
     pub fn draw(&self, _area: &GLArea) {
-        if self.aircraft_position.borrow().deref().is_some() {
+        if self.aircraft_position.borrow().is_some() {
             unsafe {
-                gl::LineWidth(1.0);
                 gl::BindBuffer(gl::ARRAY_BUFFER, self.aircraft_vertex_buffer);
                 gl::EnableVertexAttribArray(0); // this is "layout (location = 0)" in vertex shader
                 gl::VertexAttribPointer(
@@ -94,11 +93,13 @@ impl AircraftRenderer {
                     std::ptr::null(), // offset of the first component
                 );
 
-                gl::DrawArrays(gl::LINE_LOOP, 0 as GLint, 4 as GLint);
-                gl::DrawArrays(gl::LINE_LOOP, 4 as GLint, 4 as GLint);
-                gl::DrawArrays(gl::LINE_LOOP, 8 as GLint, 4 as GLint);
+                // Draw the fuselage
+                gl::DrawArrays(gl::TRIANGLES, 0 as GLint, 6 as GLint);
+                // Draw the wings
+                gl::DrawArrays(gl::TRIANGLES, 6 as GLint, 6 as GLint);
+                // Draw the tail
+                gl::DrawArrays(gl::TRIANGLES, 12 as GLint, 6 as GLint);
 
-                gl::LineWidth(1.0);
                 gl::BindBuffer(gl::ARRAY_BUFFER, 0); //Bind GL_ARRAY_BUFFER to our handle
                 gl::DisableVertexAttribArray(0); // this is "layout (location = 0)" in vertex shader
             }
@@ -128,19 +129,19 @@ impl AircraftRenderer {
         let fuselage_length = line_length * 1.0; // Full length of the fuselage
         let fuselage_width = line_length * 0.2; // Width of the fuselage
         let wing_length = line_length * 0.9; // Length of the wings
-        let wing_width = line_length * 0.2; // wing width
+        let wing_width = line_length * 0.3; // wing width
         let tail_length = line_length * 0.5; // Length of the tail section
         let tail_width = line_length * 0.15; // Width of the tail section
 
 // Calculate positions of key points based on aircraft heading
         let nose_r = aircraft_position.coordinate_at(fuselage_length / 2.0, heading).coordinate_at(fuselage_width / 2.0, (heading + 90.0) % 360.0);
-        let nose_l = nose_r.coordinate_at(fuselage_width, (heading + 270.0) % 360.0);
-        let tail_r = nose_r.coordinate_at(fuselage_length, (heading + 180.0) % 360.0);
-        let tail_l = nose_l.coordinate_at(fuselage_length, (heading + 180.0) % 360.0);
+        let nose_l = aircraft_position.coordinate_at(fuselage_length / 2.0, heading).coordinate_at(fuselage_width / 2.0, (heading + 270.0) % 360.0);
+        let tail_r = aircraft_position.coordinate_at(fuselage_length / 2.0, heading + 180.0).coordinate_at(fuselage_width / 5.0, (heading + 90.0) % 360.0);
+        let tail_l = aircraft_position.coordinate_at(fuselage_length / 2.0, heading + 180.0).coordinate_at(fuselage_width / 5.0, (heading + 270.0) % 360.0);
         let left_wing_r = aircraft_position.coordinate_at(wing_length / 2.0, (heading + 270.0) % 360.0);
-        let left_wing_f = left_wing_r.coordinate_at(wing_width, heading);
-        let right_wing_f = left_wing_f.coordinate_at(wing_length, (heading + 90.0) % 360.0);
-        let right_wing_r = left_wing_r.coordinate_at(wing_length, (heading + 90.0) % 360.0);
+        let left_wing_f = aircraft_position.coordinate_at(wing_width, (heading) % 360.0);
+        let right_wing_r = aircraft_position.coordinate_at(wing_length / 2.0, (heading + 90.0) % 360.0);
+        let right_wing_f = aircraft_position.coordinate_at(wing_width, (heading) % 360.0);
         let left_tail_r = aircraft_position.coordinate_at(fuselage_length / 2.0, (heading + 180.0) % 360.0).coordinate_at(tail_length / 2.0, (heading + 270.0) % 360.0);
         let left_tail_f = left_tail_r.coordinate_at(tail_width, heading);
         let right_tail_f = left_tail_f.coordinate_at(tail_length, (heading + 90.0) % 360.0);
@@ -160,22 +161,29 @@ impl AircraftRenderer {
         let right_tail_projected_f = projector.project(right_tail_f.get_latitude(), right_tail_f.get_longitude());
         let right_tail_projected_r = projector.project(right_tail_r.get_latitude(), right_tail_r.get_longitude());
 
-        // Fuselage (rectangle)
+        // Fuselage (two triangles)
         vertices.push(Vertex { position: nose_projected_l });
         vertices.push(Vertex { position: nose_projected_r });
         vertices.push(Vertex { position: tail_projected_r });
+        vertices.push(Vertex { position: tail_projected_r });
         vertices.push(Vertex { position: tail_projected_l });
-// Wings (rectangle)
+        vertices.push(Vertex { position: nose_projected_l });
+
+        // Wings (two triangles)
         vertices.push(Vertex { position: left_wing_projected_r });
         vertices.push(Vertex { position: right_wing_projected_r });
         vertices.push(Vertex { position: right_wing_projected_f });
+        vertices.push(Vertex { position: right_wing_projected_f });
         vertices.push(Vertex { position: left_wing_projected_f });
+        vertices.push(Vertex { position: left_wing_projected_r });
 
-// Tail (rectangle)
+        // Tail (two triangles)
         vertices.push(Vertex { position: left_tail_projected_r });
         vertices.push(Vertex { position: right_tail_projected_r });
         vertices.push(Vertex { position: right_tail_projected_f });
+        vertices.push(Vertex { position: right_tail_projected_f });
         vertices.push(Vertex { position: left_tail_projected_f });
+        vertices.push(Vertex { position: left_tail_projected_r });
 
         vertices
     }
