3f297776e172070a86f16c41c51482c82387ef2a
Trevor Campbell <trevor@shartrec.com>
2025-03-20 17:07:40 +1100

    Update for changes to libadwaita 0.7.2

--- src/window/mod.rs
+++ src/window/mod.rs
@@ -50,7 +50,7 @@ mod aircraft_renderer;
 
 glib::wrapper! {
     pub struct Window(ObjectSubclass<imp::Window>)
-        @extends gtk::ApplicationWindow, gtk::Window, gtk::Widget,
+        @extends adw::ApplicationWindow, gtk::Window, gtk::Widget,
         @implements gio::ActionGroup, gio::ActionMap, gtk::Accessible, gtk::Buildable,
                     gtk::ConstraintTarget, gtk::Native, gtk::Root, gtk::ShortcutManager;
 }
